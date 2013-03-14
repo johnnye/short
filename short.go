0151e1e23da6b4f7b9d47e90d9e5d0fa934b20ad
John Nye <john@johnnye.net>
2013-03-14 21:33:28 +0000

    formatted the code

--- short.go
+++ short.go
@@ -1,35 +1,39 @@
 package main
+
 /*
 * My in memory link shortner written in Go 
 * This is released under a "you'd be mad to use it" license 
 * 
 * John Nye
 *
-*/
+ */
 import (
+	"./utils"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
-	"./utils"
 )
+
 const domain = "http://localhost:8080/"
+
 type Data struct {
 	Original  string
 	Short     string
 	FullShort string
-	HitCount int
+	HitCount  int
 }
+
 var collection []Data
+
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	type NewURL struct {
 		URL string
 	}
 	var url NewURL
-	
 
 	create, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -38,14 +42,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	if len(create) == 0 {
 		long := getLongURL(r.URL.Path[1:])
-		if(len(long)>0){
+		if len(long) > 0 {
 			http.Redirect(w, r, long, http.StatusFound)
 			return
 		}
-		http.Redirect(w,r,domain,http.StatusNotFound)
+		http.Redirect(w, r, domain, http.StatusNotFound)
 		return
 	}
-	
+
 	err = json.Unmarshal(create, &url)
 
 	if err != nil {
@@ -54,7 +58,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	newItem := createShortURL(url.URL)
-	collection = append(collection, newItem )
+	collection = append(collection, newItem)
 
 	output, err := json.Marshal(newItem)
 
@@ -67,16 +71,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", output)
 }
 
-func createShortURL(url string) Data{
-	for _,element := range collection {
-		if(element.Original == url){
+func createShortURL(url string) Data {
+	for _, element := range collection {
+		if element.Original == url {
 			return element
 		}
 	}
 	var d Data
 	encodedVar := base62.StdEncoding.EncodeToString([]byte(string(len(collection))))
 	encodedVar = strings.Trim(encodedVar, "=")
-	d.FullShort = strings.Join([]string{domain,encodedVar}, "")
+	d.FullShort = strings.Join([]string{domain, encodedVar}, "")
 	d.Short = encodedVar
 	d.Original = url
 	d.HitCount = 0
@@ -85,9 +89,9 @@ func createShortURL(url string) Data{
 	return d
 }
 
-func getLongURL(short string)string{
+func getLongURL(short string) string {
 	for _, element := range collection {
-		if (element.Short == short){
+		if element.Short == short {
 			return element.Original
 		}
 	}
